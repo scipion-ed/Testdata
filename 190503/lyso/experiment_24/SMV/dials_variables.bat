da839d7b379a057a97bcb4cbd3736e5cb56b5980
ViktorEGBengtsson <viktor.bengtsson@mmk.su.se>
2020-03-19 19:52:04 +0100

    Add additional test dataset

---
+++ 190503/lyso/experiment_24/SMV/dials_variables.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set scan_range=scan_range=1,36
+set exclude_images=exclude_images=
+set rotation_axis=geometry.goniometer.axes=-0.6204,-0.7843,0.0000
+
+:: To run:
+::     call dials_variables.bat
+::
+::     dials.import directory=data %rotation_axis%
+::     dials.find_spots datablock.json %scan_range%
+::     dials.integrate %exclude_images% refined.pickle refined.json
---
+++ 190503/lyso/experiment_24/SMV/dials_variables.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+scan_range='scan_range=1,36'
+exclude_images='exclude_images='
+rotation_axis='geometry.goniometer.axes=-0.6204,-0.7843,0.0000'
+#
+# To run:
+#     source dials_variables.sh
+#
+# and:
+#     dials.import directory=data $rotation_axis
+#     dials.find_spots datablock.json $scan_range
+#     dials.integrate $exclude_images refined.pickle refined.json
+#
